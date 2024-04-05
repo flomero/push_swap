edf1e39d74d98c931d74698fdc05d1423946c388
flomero <49640251+flomero@users.noreply.github.com>
2024-04-05 17:42:18 +0200

    make sort three not depend on values

--- ft_sort_three.c
+++ ft_sort_three.c
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:36:13 by flfische          #+#    #+#             */
-/*   Updated: 2024/04/04 12:57:32 by flfische         ###   ########.fr       */
+/*   Updated: 2024/04/05 17:40:52 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,18 @@ void	ft_sort_three(t_push_swap *ps)
 	int	*arr;
 
 	arr = ps->a.stack;
-	if (arr[0] == 0 && arr[1] == 2 && arr[2] == 1)
+	if (arr[0] < arr[1] && arr[2] > arr[0])
 	{
 		ft_swap_a(ps);
 		ft_rot_a(ps);
 	}
-	else if (arr[0] == 1 && arr[1] == 0 && arr[2] == 2)
+	else if (arr[0] > arr[1] && arr[2] > arr[0])
 		ft_swap_a(ps);
-	else if (arr[0] == 1 && arr[1] == 2 && arr[2] == 0)
+	else if (arr[0] < arr[1] && arr[2] < arr[0])
 		ft_revrot_a(ps);
-	else if (arr[0] == 2 && arr[1] == 0 && arr[2] == 1)
+	else if (arr[0] > arr[1] && arr[2] > arr[1])
 		ft_rot_a(ps);
-	else if (arr[0] == 2 && arr[1] == 1 && arr[2] == 0)
+	else if (arr[0] > arr[1] && arr[2] < arr[1])
 	{
 		ft_swap_a(ps);
 		ft_revrot_a(ps);
